b16a827db94e2416a5e28fc7e1e360069d219c2c
HaHavn94 <hanna.myllarinen7994@gmail.com>
2023-02-19 22:33:31 +0200

    ndndn

--- src/App.js
+++ src/App.js
@@ -2,23 +2,24 @@ import './App.css'
 import Sidebar from './Components/Sidebar'
 import Training from './Components/Training'
 import Customers from './Components/Customers'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Calendar from './Components/Calendar'
 import Chart from './Components/Chart'
 
 export default function App() {
   return (
-    <div>     
-<BrowserRouter> 
- <Sidebar>
-     <Routes>
-            <Route path="/"           element={<Customers/>} > </Route>
+    <div>
+      <h1>Welcome</h1>
+      <BrowserRouter>
+        <Sidebar>
+          <Routes>
+            <Route path="/" element={<Customers />} > </Route>
             <Route path="/trainings" element={<Training />} >  </Route>
             <Route path="/calendar" element={<Calendar />} >  </Route>
-            <Route path="/barchart"  element={<Chart/>} >  </Route>
-     </Routes>
-  </Sidebar>
-</BrowserRouter>   
-</div>
+            <Route path="/barchart" element={<Chart />} >  </Route>
+          </Routes>
+        </Sidebar>
+      </BrowserRouter>
+    </div>
   )
 }
